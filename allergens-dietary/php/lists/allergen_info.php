441c442227f33dc0272d1dd82e67af604a7ff6fd
Verdi-B <151911787+Verdi-B@users.noreply.github.com>
2024-12-06 09:38:51 +0100

    Renaming (#1)
    * renamed all the classes and file hierarchy
    
    * changed authors to company
    
    * tested and added the renamed translations
    When there was a bug it was fixxed with a solution
    
    * removed premium features
    
    * renamed all the classes and file hierarchy
    
    * changed authors to company
    
    * tested and added the renamed translations
    When there was a bug it was fixxed with a solution
    
    ---------
    
    Co-authored-by: TimKleuver <tim.kleuver@ictoria.nl>

---
+++ allergens-dietary/php/lists/allergen_info.php
@@ -0,0 +1,67 @@
+<?php
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+/**
+ * @class Allergens_Dietary_License_Info
+ * @brief Class that creates the info
+ * the user can see the info
+ * @author Ictoria
+ * @date 12-9-2024
+ * @since 1.0.0
+ */
+
+class Allergens_Dietary_Info
+{
+    private static ?self $_instance = null;
+
+
+
+    public function showInfo()
+    {
+
+        Allergens_Dietary_Activator::load_style();
+        Allergens_Dietary_Activator::enqueue_styles();
+        
+        //flexbox voor tabs
+        $html = '<div id="info_grid" class="nav-tab-wrapper">';
+        $html .= '<div><h1 class="premium">' . __("PREMIUM  [Requires licence]", "allergens-dietary") . '</h1>';
+        $html .= '<ol>';
+        $html .= '<li class="contains">' . __('Updating allergies', 'allergens-dietary') . '</li>';
+        $html .= '<li class="contains">' . __("Changing allergy themes", "allergens-dietary") . '</li>';
+        $html .= '<li class="contains">' . __("Deleting allergies", "allergens-dietary") . '</li>';
+        $html .= '<li class="contains">' . __("Adding allergies", "allergens-dietary") . '</li>';
+        $html .= '<li class="contains">' . __("Custom look on product", "allergens-dietary") . '</li>';
+        $html .= '<li class="contains">' . __("Custom look in store", "allergens-dietary") . '</li>';
+        $html .= ' </ol>';
+        $html .= ' </div>';
+        $html .= '<div><h1 class="free">' . __("FREE VERSION  [Standard]", "allergens-dietary") . '</h1>';
+        $html .= ' <ol>';
+        $html .= '<li class="contains">' . __("Connecting allergies to products", "allergens-dietary") . '</li>';
+        $html .= '<li class="contains">' . __("Wordpress theme fiendly styles", "allergens-dietary") . '</li>';
+        $html .= '<li class="contains">' . __("An allergen overview", "allergens-dietary") . '</li>';
+        $html .= '<li class="contains">' . __("Turning the use of allergies on/off", "allergens-dietary") . '</li>';
+        $html .= '      </ol>';
+        $html .= '  </div>';
+        $html .= '  </div>';
+        $html .= ' <br> <br>';
+        echo $html;
+        //moet nog aangepast worden in css
+    }
+
+    public static function getInstance()
+    {
+        if (self::$_instance === null) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+
+    public static function getStyles()
+    {
+        wp_register_style('allergens-dietary-css', plugins_url(ALLERGENS_DIETARY_NAME . '/assets/css/allergens-dietary.css'));
+        wp_enqueue_style('allergens-dietary-admin-css', plugins_url('assets/css/allergens-dietary.css', ALLERGENS_DIETARY_FILE));
+    }
+}
