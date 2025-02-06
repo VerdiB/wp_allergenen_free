6646dbb9079e28ba33dcb7f1fd74463268a53a4c
odinVos <1197607@student.roc-nijmegen.nl>
2025-02-06 10:30:40 +0100

    Quick fix

--- allergens-dietary/allergens-dietary.php
+++ allergens-dietary/allergens-dietary.php
@@ -90,7 +90,7 @@ class load_language
 	public function __construct()
 	{
 		add_action('init', array($this, 'translation_init'));
-		add_action('plugins_loaded', array($this, 'translation_init'));
+		// add_action('plugins_loaded', array($this, 'translation_init'));
 	}
 
 
